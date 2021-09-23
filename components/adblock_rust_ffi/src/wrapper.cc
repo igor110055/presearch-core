29a9aaea32295d1f3f96fc3bd80c79df03a7d26e
mamylinx <mamy.else@gmail.com>
2021-09-23 22:28:29 +0300

    Fix adblock_rust_ffi related issue

--- components/adblock_rust_ffi/src/wrapper.cc
+++ components/adblock_rust_ffi/src/wrapper.cc
@@ -3,11 +3,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "wrapper.h"  // NOLINT https://github.com/brave/brave-browser/issues/14821
+#include "wrapper.h"
 #include <iostream>
 
 extern "C" {
-#include "lib.h"  // NOLINT
+#include "lib.h"
 }
 
 namespace adblock {
@@ -43,9 +43,6 @@ FilterList::~FilterList() {}
 Engine::Engine() : raw(engine_create("")) {}
 
 Engine::Engine(const std::string& rules) : raw(engine_create(rules.c_str())) {}
-
-Engine::Engine(const char* data, size_t data_size)
-    : raw(engine_create_from_buffer(data, data_size)) {}
 
 void Engine::matches(const std::string& url,
                      const std::string& host,
