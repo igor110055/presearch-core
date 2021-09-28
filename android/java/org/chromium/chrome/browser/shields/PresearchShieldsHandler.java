a01263c4526a0e083ee52162acb2282f2c807aea
mamylinx <mamy.else@gmail.com>
2021-09-28 09:49:01 +0300

    Trying to fix adblock issue

--- android/java/org/chromium/chrome/browser/shields/PresearchShieldsHandler.java
+++ android/java/org/chromium/chrome/browser/shields/PresearchShieldsHandler.java
@@ -709,7 +709,8 @@ public class PresearchShieldsHandler implements PresearchRewardsHelper.LargeIcon
             public void onCheckedChanged(CompoundButton buttonView,
                                          boolean isChecked) {
                 if (0 != mHost.length()) {
-                    PresearchShieldsContentSettings.setShields(mProfile, mHost, PresearchShieldsContentSettings.RESOURCE_IDENTIFIER_ADS_TRACKERS, isChecked, false);
+                    // PresearchShieldsContentSettings.setShields(mProfile, mHost, PresearchShieldsContentSettings.RESOURCE_IDENTIFIER_ADS_TRACKERS, isChecked, false);
+                    PresearchShieldsContentSettings.setShields(mProfile, mHost, PresearchShieldsContentSettings.RESOURCE_IDENTIFIER_JAVASCRIPTS, isChecked, false);
                     if (null != mMenuObserver) {
                         mMenuObserver.onMenuTopShieldsChanged(isChecked, false);
                     }
